818c9ecdf6ca9dff12e19ec42aed951a6bc0b068
Adit K <aditkirtani@gmail.com>
2017-04-05 16:47:17 +0530

    One question done

--- t16/Q4.java
+++ t16/Q4.java
@@ -10,15 +10,34 @@ public class Q4 {
 	public static void main(String[] args) {
 		Q4 q = new Q4();
 		q.input();
+		q.calculate();
+		q.display();
 	}
 	
+	private void display() {
+		System.out.println("FINAL BILL");
+		System.out.println("Book name " + Bname);
+		System.out.println("Discounted Price: " + price);
+	}
+
+	private void calculate() {
+		if (price <= 1000) {
+			price = price * 2/100;
+		} else if (price <= 3000) {
+			price = price * 1/10;
+		} else {
+			price = price * 3/20;
+		}
+	}
+
 	private void input() {
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("ENter book name");
+		System.out.println("Enter book name");
 		Bname = in.nextLine();
 		
-		System.out.println();
+		System.out.println("Enter book price");
+		price = in.nextDouble();
 		
 		in.close();
 	}
