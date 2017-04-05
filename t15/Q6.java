e760550a3c4119bc7f6902395f7039b48993d29b
Adit K <aditkirtani@gmail.com>
2017-04-05 22:41:01 +0530

    Q6 almost done

--- t15/Q6.java
+++ t15/Q6.java
@@ -1,9 +1,22 @@
 package t15;
 
+import java.util.Scanner;
+
 public class Q6 {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.println("Enter number of students");
+		int n = in.nextInt();
+		
+		String[] names = new String[n];
+		int[] rNum = new int[n];
+		int[] sub1 = new int[n];
+		int[] sub2 = new int[n];
+		int[] sub3 = new int[n];
+		
+		in.close();
 	}
 	
 }
