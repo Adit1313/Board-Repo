f058c3017adde4b3e76c9606a3598fcd81523b7b
Adit K <aditkirtani@gmail.com>
2017-04-06 11:11:14 +0530

    Q5 done

--- t14/Q5.java
+++ t14/Q5.java
@@ -1,9 +1,33 @@
 package t14;
 
+import java.util.Scanner;
+
 public class Q5 {
 
 	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
 		 
+		 System.out.println("Enter a number to check if it is a special number");
+		 int a = in.nextInt();
+		 
+		 String b = String.valueOf(a);
+		 char[] digits = b.toCharArray();
+		 
+		 int sum = 0;
+		 int product = 1;
+		 
+		 for (int i = 0; i < digits.length; i++) {
+			 sum += Integer.valueOf(String.valueOf(digits[i]));
+			 product *= Integer.valueOf(String.valueOf(digits[i]));
+		 }
+		 
+		 if (sum + product == a) {
+			 System.out.println(a + " is a special number");
+		 } else {
+			 System.out.println(a + " is not a special number");
+		 }
+		 
+		 in.close();
 	}
 
 } 
